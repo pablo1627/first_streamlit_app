7e856bbd6022da0c68855016fd8dc289593e631a
Pablo García <121629210+pablo1627@users.noreply.github.com>
2023-01-10 03:58:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 # Display the table on the page.
 #streamlit.dataframe(my_fruit_list)
 
-streamlit.stop
+
 
 #def get_fruityvice_data(this_fruit_choice):
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
@@ -48,7 +48,7 @@ try:
 
 
 
-
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
